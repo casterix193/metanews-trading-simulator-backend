c5546034c7200bd22b0e46c29a707498e68f8d53
Dharmil Desai <42259391+casterix193@users.noreply.github.com>
2022-12-14 12:56:14 +0200

    basic setup

---
+++ app/config/auth.config.js
@@ -0,0 +1,5 @@
+// JWT secret chosen randomly
+module.exports = {
+    secret: "CrofMJdo0FHnxbk0sqS9o8qy6jVhv3NjeEBLsLmur53RLzkVBm"
+};
+
---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+// Mongodb database connection parameters
+
+module.exports = {
+    HOST: "dharmil371:admin123@nextjscrud.qawa92r.mongodb.net",
+    PORT: 27017,
+    DB: "papertrade_db"
+};
